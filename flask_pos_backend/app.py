1e5074dcb5e9ce5428177c62b0c6f1ed8cb95238
Fredrick Mwangi <fredmallin49@gmail.com>
2026-02-18 14:26:58 +0300

    initialize my project

--- flask_pos_backend/app.py
+++ flask_pos_backend/app.py
@@ -14,9 +14,10 @@ app.config["SECRET_KEY"] = "supersecretkey"
 # ---------------- CORS ----------------
 CORS(
     app,
-    resources={r"/api/*": {"origins": ["https://pos-gadget-source-c5lo.vercel.app/", "https://pos-gadget-source-c5lo.vercel.app/"]}},
+    resources={r"/api/*": {"origins": "https://pos-gadget-source-c5lo.vercel.app/"}},
     supports_credentials=True
 )
+
 
 DB_PATH = "pos.db"
 
