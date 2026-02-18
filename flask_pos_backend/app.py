509cd16983601086a37e5ec63be11f9664bfa184
Fredrick Mwangi <fredmallin49@gmail.com>
2026-02-18 14:21:58 +0300

    initialize my project

--- flask_pos_backend/app.py
+++ flask_pos_backend/app.py
@@ -363,4 +363,4 @@ def home():
     return jsonify({"message": "Flask POS Backend is running"}), 200
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
