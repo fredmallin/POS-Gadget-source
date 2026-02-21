38aeaa022edcaaafd3db2f918a6274ed770a649d
Fredrick Mwangi <fredmallin49@gmail.com>
2026-02-21 15:21:22 +0300

    initialize my app.py

--- flask_pos_backend/app.py
+++ flask_pos_backend/app.py
@@ -13,21 +13,16 @@ app.config["SECRET_KEY"] = "supersecretkey"
 from flask import request
 from flask_cors import CORS
 
-def get_cors_origin():
-    allowed_origins = [
-        "http://localhost:5173",
-        "https://pos-gadget-source-4.onrender.com", 
-        "https://pos-gadget-source-c5lo.vercel.app",
-        "https://pos-gadget-source-xcwz.vercel.app"
-    ]
-    origin = request.headers.get("Origin")
-    if origin in allowed_origins:
-        return origin
-    return None
+from flask_cors import CORS
 
 CORS(
     app,
-    resources={r"/api/*": {"origins": get_cors_origin}},
+    resources={r"/api/*": {"origins": [
+        "http://localhost:5173",
+        "https://pos-gadget-source-4.onrender.com",
+        "https://pos-gadget-source-c5lo.vercel.app",
+        "https://pos-gadget-source-xcwz.vercel.app"
+    ]}},
     supports_credentials=True
 )
 
